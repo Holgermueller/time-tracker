fdff783f197a44e3717e93f99abbc4d560bfbf51
holgermueller <9jholger@gmail.com>
2024-05-12 15:15:00 -0500

    added images to cards

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const showDaily = () => {
+  console.log("daily");
+};
+
+const showWeekly = () => {
+  console.log("weekly");
+};
+
+const showMonthly = () => {
+  console.log("monthly");
+};
