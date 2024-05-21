43cbb7d5c2614ff0ce8e1ad45ad09713b468747a
holgermueller <9jholger@gmail.com>
2024-05-21 16:32:51 -0500

    got functions to load data to dom working

--- index.js
+++ index.js
@@ -3,22 +3,66 @@
 async function fetchJsonFile() {
   const response = await fetch("data.json");
   const data = await response.json();
+
+  document.getElementById("workCurrent").innerHTML =
+    data[0].timeframes.daily.current + "hrs";
+  document.getElementById("workPrevious").innerHTML =
+    "Last week - " + data[0].timeframes.daily.previous + "hrs";
+
+  document.getElementById("playCurrent").innerHTML =
+    data[1].timeframes.daily.current + "hrs";
+  document.getElementById("playPrevious").innerHTML =
+    "Last week - " + data[1].timeframes.daily.previous + "hrs";
+
   console.log(data);
 }
 
-const loadDaily = () => {
-  // This will load on page load.
+const showDaily = async () => {
+  const response = await fetch("data.json");
+  const data = await response.json();
+
+  document.getElementById("workCurrent").innerHTML =
+    data[0].timeframes.daily.current + "hrs";
+  document.getElementById("workPrevious").innerHTML =
+    "Last week - " + data[0].timeframes.daily.previous + "hrs";
+
+  document.getElementById("playCurrent").innerHTML =
+    data[1].timeframes.daily.current + "hrs";
+  document.getElementById("playPrevious").innerHTML =
+    "Last week - " + data[1].timeframes.daily.previous + "hrs";
 };
 
-const showDaily = () => {
-  console.log("daily");
-};
+const showWeekly = async () => {
+  const response = await fetch("data.json");
+  const data = await response.json();
 
-const showWeekly = () => {
+  document.getElementById("workCurrent").innerHTML =
+    data[0].timeframes.weekly.current + "hrs";
+  document.getElementById("workPrevious").innerHTML =
+    "Last week - " + data[0].timeframes.weekly.previous + "hrs";
+
+  document.getElementById("playCurrent").innerHTML =
+    data[1].timeframes.weekly.current + "hrs";
+  document.getElementById("playPrevious").innerHTML =
+    "Last week - " + data[1].timeframes.weekly.previous + "hrs";
+
   console.log("weekly");
 };
 
-const showMonthly = () => {
+const showMonthly = async () => {
+  const response = await fetch("data.json");
+  const data = await response.json();
+
+  document.getElementById("workCurrent").innerHTML =
+    data[0].timeframes.monthly.current + "hrs";
+  document.getElementById("workPrevious").innerHTML =
+    "Last week - " + data[0].timeframes.monthly.previous + "hrs";
+
+  document.getElementById("playCurrent").innerHTML =
+    data[1].timeframes.monthly.current + "hrs";
+  document.getElementById("playPrevious").innerHTML =
+    "Last week - " + data[1].timeframes.monthly.previous + "hrs";
+
   console.log("monthly");
 };
 
